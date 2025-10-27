d98e3d8555ca55ce67bc13c062fb3ea04390f5a3
sankargongati <gds21103@gmail.com>
2025-10-28 03:31:29 +0530

    Update config.js

--- js/admin/config.js
+++ js/admin/config.js
@@ -2,7 +2,8 @@
 export const SUPABASE_URL = 'https://gswdqdsviqrzsoahxapc.supabase.co/';
 export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdzd2RxZHN2aXFyenNvYWh4YXBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4ODM5NzcsImV4cCI6MjA3NjQ1OTk3N30.JFF6NFIdqZAE9ioHeMdfIVpN84UtN_Z0K2pZYodlpZY';
 export const INVITE_USER_FUNCTION_URL = `${SUPABASE_URL}/functions/v1/invite-user`;
-
+export const SUPABASE_ID = 'gswdqdsviqrzsoahxapc';
+export const SUPABASE_DASHBOARD_URL = `https://supabase.com/dashboard/project/${SUPABASE_ID}`;
 export const GALLERY_BUCKET = 'gallery-images';
 export const BLOG_BUCKET = 'blog-images';
 export const MEMBERS_BUCKET = 'member-images';
